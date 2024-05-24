ef3def7142aacd811f1b6a748f5fd69ad8e72af9
ALEJANDRORODRIGUEZ <usuariolg80@gmail.com>
2024-05-23 19:31:05 -0500

    se creo el primer punto de la tarea 4

---
+++ alejandro-rodriguez-3115276934/tarea 4/main.js
@@ -0,0 +1,10 @@
+let numero=0;
+for(let contador= 1; contador <= 50; contador++)
+  {numero += 1;
+  console.log(numero);
+  }
+
+
+/* este algoritmo se hizo creando un bucle que se repite 50 veces y se va imprimiendo 1 a 1 cada iteracion, se creo una variable 
+let numero que inicia en cero, y qye en cada iteracion va sumando de a 1 */
+
