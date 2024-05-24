524014674fa4169410217c820a047df20e1d34e1
Julianga <jrgavirialugo@gmail.com>
2024-05-23 22:17:10 -0500

    se incluye el método startsWith para solo contar los que empiezan en dos

--- julian-gaviria-1121917910/tarea_4/tarea_4.js
+++ julian-gaviria-1121917910/tarea_4/tarea_4.js
@@ -1,5 +1,9 @@
 //tarea numero 4 nodulo 3 by juliangav
 
 for (let index = 1; index <= 2000; index++) {
-    console.log(index)
+    const convert = index.toString()
+    if (convert.startsWith("2")) {
+        console.log(convert)
+    }
+    
 }
