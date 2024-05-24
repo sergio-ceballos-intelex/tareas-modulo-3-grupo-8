c39b03e09bd1d0e7e298edc032ec44bb95632eae
MiguelMendez234 <miguelmendeza1215@gmail.com>
2024-05-24 17:06:42 -0500

    Modificacion que muestra solo los numeros del 2 al 2000 empezados por 2

--- Miguel-Mendez-3219038111/Tarea-4/main.js
+++ Miguel-Mendez-3219038111/Tarea-4/main.js
@@ -1,3 +1,8 @@
-for (let i = 1; i <= 2000; i ++){
-    console.log(i)
+for (let i = 2; i <= 2000; i ++){
+    let num= i.toString()
+    let numStart= num.startsWith("2")
+    if (numStart==true){
+        console.log(i)
+    }
+    // console.log(num.startsWith("2"))
 }
