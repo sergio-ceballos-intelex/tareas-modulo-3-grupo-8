36d64ab2912d842580a3cdf3c421ca3770780c01
Juan Diego Sánchez <Jdsch10.diegosanches11@gmail.com>
2024-05-24 19:47:29 -0500

    Se modifica el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)

--- Juan_Sanchez_3117286450/Tarea4/script.js
+++ Juan_Sanchez_3117286450/Tarea4/script.js
@@ -1,5 +1,7 @@
 let numero;
 
-for(let numero = 1; numero <= 2000; numero ++){
+for (let numero = 1; numero <= 2000; numero++) {
+    if (numero.toString().startsWith('2')) {
         console.log(numero);
-    }
+    };
+};
