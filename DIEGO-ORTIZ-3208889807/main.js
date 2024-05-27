7b454d8009ff290a50c6f7a07a7a09ddc5244126
Diego Ortiz <dmortizu@gmail.com>
2024-05-27 17:10:13 -0500

    Se modifica el archivo anterior y se imprime los números del 1 al 2000.

--- DIEGO-ORTIZ-3208889807/main.js
+++ DIEGO-ORTIZ-3208889807/main.js
@@ -1,4 +1,4 @@
 //Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
-for (var i = 1; i <=50; i++){
+for (var i = 1; i <=2000; i++){
     console.log(i);
 }
