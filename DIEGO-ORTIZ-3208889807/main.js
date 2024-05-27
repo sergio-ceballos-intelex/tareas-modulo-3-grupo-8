17c3bae53e0d35d777767b5f1809f3583b5b9f60
Diego Ortiz <dmortizu@gmail.com>
2024-05-27 17:20:34 -0500

    Se modifica el archivo main.js para mostrar los números que empiezan por 2

--- DIEGO-ORTIZ-3208889807/main.js
+++ DIEGO-ORTIZ-3208889807/main.js
@@ -1,4 +1,6 @@
 //Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
 for (var i = 1; i <=2000; i++){
-    console.log(i);
+    if (i.toString().startsWith('2')){
+        console.log(i);
+    }
 }
