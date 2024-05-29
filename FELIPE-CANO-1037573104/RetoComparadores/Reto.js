b2318281e9124b69cfddc23b0a47e13076ba4910
Felipe Cano <felipecanocarvajal@gmail.com>
2024-05-28 23:14:20 -0500

    Impresion en pantalla numeros del 1 al 50

---
+++ FELIPE-CANO-1037573104/RetoComparadores/Reto.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 15;
+
+console.log("El numero1 es igual al numero2?", num1 === num2);
+console.log("El numero1 es diferente al numero2?", num1 !== num2);
+console.log("El numero2 es mayor que el numero1?", num2 > num1);
+console.log("El numero1 es menor que el numero2?", num1 < num2);
+console.log("El numero1 es mayor o igual que el numero2?", num1 >= num2);
+console.log("El numero2 es menor o igual que  numero1?", num2 <= num1);
+
+
--- FELIPE-CANO-1037573104/Tarea4/Tarea4.js
+++ FELIPE-CANO-1037573104/Tarea4/Tarea4.js
@@ -1,7 +1,9 @@
-let myArray=[];
+//Ejercicio 1  --- Impresion de numeros del 1 al 50 *************************************************************************************
+
+let myArray1=[];
 
 for(let inicio=1;inicio<=50;inicio++){
     const numero=inicio;
-    myArray.push(numero);
+    myArray1.push(numero);
 }
-console.log("Numeros del 1 al 50", myArray);
+console.log("Numeros del 1 al 50", myArray1);
