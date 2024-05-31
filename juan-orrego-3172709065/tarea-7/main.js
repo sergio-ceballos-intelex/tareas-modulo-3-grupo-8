4a73ab5d7279074310393bdaeaca08ab7149cc98
Juan_Diego <juan3312orrego@gmail.com>
2024-05-31 17:35:21 -0500

    1. Ejercicio de Bucle for cón Arrays.

---
+++ juan-orrego-3172709065/tarea-7/main.js
@@ -0,0 +1,11 @@
+// Manipulación de objetos y arrays 
+
+// 1. Ejercicio de Bucle For con Arrays:
+let numeros = [1, 2, 3, 4, 5];
+let numMul = [];
+
+for(i = 0; i < numeros.length; i++) {
+    numMul.push(numeros[i]*2);
+}
+
+console.log(numMul);
