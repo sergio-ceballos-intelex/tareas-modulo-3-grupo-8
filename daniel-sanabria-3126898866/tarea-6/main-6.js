66eedc4af98fd5ede63d39007d45a8e36fe93688
Pininita <pininita92001@gmail.com>
2024-05-31 10:17:30 -0500

    corrigiendo puntos tarea-6

--- daniel-sanabria-3126898866/tarea-6/main-6.js
+++ daniel-sanabria-3126898866/tarea-6/main-6.js
@@ -3,7 +3,7 @@
 let numeros = [1,2,3,4,5];
 let numerosPor2= [];
 
-for(let position = 0; position <= numeros.length -1; position++){
+for(let position = 0; position < numeros.length; position++){
     let numerosx = numeros[position]*2;
     numerosPor2.push(numerosx)
 }
@@ -12,23 +12,22 @@ console.log(numerosPor2);
 // task 2
 
 let productos = [
-  { nombre: "Camisa", precio: 20 },
+  { nombre: "Camisa", precio: 100 },
   { nombre: "Pantalón", precio: 30 },
   { nombre: "Zapatos", precio: 50 },
 ];
 
-let lowestProductAndPrice = [];
-let precioMasBajo = Infinity; // se usa para asegurar el precio mas bajo
 
-for(let eachElement = 0; eachElement <= productos.length -1; eachElement++){
-  if( productos[eachElement].precio < precioMasBajo){ // se pone .precio porque lo que estamos comparando es el precio dentro del array
-      precioMasBajo = productos[eachElement].precio
-      lowestProductAndPrice.push(productos[eachElement])
+let productoPrecioMasBajo = productos[0]; // se usa para asegurar el precio mas bajo
+
+for(let i = 0; i < productos.length ; i++){
+  if( productos[i].precio < productoPrecioMasBajo.precio){ // se pone .precio porque lo que estamos comparando es el precio dentro del array
+    productoPrecioMasBajo = productos[i]
+     
   }
-  
 }
 
-console.log(lowestProductAndPrice);
+console.log(productoPrecioMasBajo);
 
 // task 3
 
