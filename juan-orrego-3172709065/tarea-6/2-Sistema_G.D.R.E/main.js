d376c4015a86489e49055a7604aa04f69b380c78
Juan_Diego <juan3312orrego@gmail.com>
2024-05-30 20:06:26 -0500

    Se actualizo el main del Sistema de gestión de registro de estudiantes

--- juan-orrego-3172709065/tarea-6/2-Sistema_G.D.R.E/main.js
+++ juan-orrego-3172709065/tarea-6/2-Sistema_G.D.R.E/main.js
@@ -1,10 +1,15 @@
+// Ubicar el boton y añadir evento
 const btnRegistro = document.getElementById("btnRegistro");
-
-const mostrarMensaje = document.getElementById("confirRegistro").style.display = "flex";
-const mensaje = mostrarMensaje + 
-
 btnRegistro.addEventListener("click",aggDatos);
 
+// Funcion para mostrar en pantalla resultado de registro
+function mensaje(texto){
+    document.getElementById("confirRegistro").style.display = "flex";
+    document.getElementById("onScreen").innerHTML = texto;
+    return texto;
+}
+
+// Funcion para agregar datos del estudiante
 function aggDatos(){
     const estudiante = {
         nombre: prompt("Digite su nombre:"),
@@ -16,29 +21,23 @@ function aggDatos(){
     }
 
     if(estudiante.edad < 18){
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = "¡Ups! Eres menor de edad no puedes registrarte.";
+        mensaje("¡Ups! Eres menor de edad no puedes registrarte.");
     }else if(estudiante.edad >= 18 && estudiante.prom < 6){
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = "¡Ups! ¡No cumples cón el promedio requerido!.";
+        mensaje("¡Ups! ¡No cumples cón el promedio requerido!.");
     }else if(estudiante.edad >= 18 && estudiante.prom >= 6 && estudiante.estReg === "no"){
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = "¡Debes registrarte como estudiante regular!.";
+        mensaje("¡Debes registrarte como estudiante regular!.");
     }else if(estudiante.edad >= 18 && estudiante.prom >= 6 && estudiante.estReg === "si" && estudiante.estBecado === "no"){
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = "¡Puedes solicitar una beca!.";
+        mensaje("¡Puedes solicitar una beca!.");
     }else if(estudiante.edad >= 18 && estudiante.prom >= 6 && estudiante.estReg === "si" && estudiante.estBecado === "si"){
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = `Felicidades, ${estudiante.nombre}! Cumples con todos los requisitos. :)<br><br>
+        mensaje(`Felicidades, ${estudiante.nombre}! Cumples con todos los requisitos. :)<br><br>
         <ul>
           <li>Tienes ${estudiante.edad} años, cumples con la edad mínima.</li>
           <li>Tu promedio es ${estudiante.prom}, cumples con el promedio mínimo.</li>
           <li>Eres un estudiante regular.</li>
           <li>Has aprobado ${estudiante.materiasAprob} materias.</li>
           <li>Eres un estudiante becado.</li>
-        </ul>`;
+        </ul>`);
     }else{
-        document.getElementById("confirRegistro").style.display = "flex";
-        document.getElementById("onScreen").innerHTML = "¡Ups! Digitaste un dato invalido.";
+        mensaje("¡Ups! Digitaste un dato invalido.");
     }
 }
