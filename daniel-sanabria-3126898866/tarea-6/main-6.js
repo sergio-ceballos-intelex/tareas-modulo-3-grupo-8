673ec232a9468b904d7916b1107339f74b5bd95f
Pininita <pininita92001@gmail.com>
2024-05-30 20:03:16 -0500

    terminando punto-3 tarea-6

--- daniel-sanabria-3126898866/tarea-6/main-6.js
+++ daniel-sanabria-3126898866/tarea-6/main-6.js
@@ -11,21 +11,42 @@
 
 // task 2
 
-let productos = [
-  { nombre: "Camisa", precio: 20 },
-  { nombre: "Pantalón", precio: 30 },
-  { nombre: "Zapatos", precio: 50 },
+// let productos = [
+//   { nombre: "Camisa", precio: 20 },
+//   { nombre: "Pantalón", precio: 30 },
+//   { nombre: "Zapatos", precio: 50 },
+// ];
+
+// let lowestProductAndPrice = [];
+// let precioMasBajo = Infinity; // se usa para asegurar el precio mas bajo
+
+// for(let eachElement = 0; eachElement <= productos.length -1; eachElement++){
+//   if( productos[eachElement].precio < precioMasBajo){ // se pone .precio porque lo que estamos comparando es el precio dentro del array
+//       precioMasBajo = productos[eachElement].precio
+//       lowestProductAndPrice.push(productos[eachElement])
+//   }
+  
+// }
+
+// console.log(lowestProductAndPrice);
+
+// task 3
+
+let libros = [
+  { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+  { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+  { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
 ];
 
-let lowestProductAndPrice = [];
-let precioMasBajo = Infinity; // se usapara asegurar el precio mas bajo
+let newBook = []
 
-for(let eachElement = 0; eachElement <= productos.length -1; eachElement++){
-  if( productos[eachElement].precio < precioMasBajo){ // se pone .precio porque lo que estamos comparando es el precio dentro del array
-      precioMasBajo = productos[eachElement].precio
-      lowestProductAndPrice.push(productos[eachElement])
+for(let i = 0; i <= libros.length -1; i++){
+  let letraEl = libros[i];
+  if(letraEl.titulo.includes("El")){
+    newBook.push(letraEl);
   }
-  
-}
+}console.log(newBook);
 
-console.log(lowestProductAndPrice);
+// task 4
+
+
