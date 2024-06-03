7219155449aed528d8867d56f6de4cb56c521028
Pininita <pininita92001@gmail.com>
2024-06-03 12:19:13 -0500

    terminando tarea-7

--- daniel-sanabria-3126898866/tarea-7/main7.js
+++ daniel-sanabria-3126898866/tarea-7/main7.js
@@ -58,32 +58,130 @@ const estudiantes = [
 
 // TASK 3
 
-const estudiantes2 = [
+const estudiantes3 = [
     { nombre: "Camilo", edad: 33, promedio: 10 },
     { nombre: "Juan", edad: 30, promedio: 6 },
     { nombre: "Andres", edad: 29, promedio: 8.7 },
 ];
-// Calcula el promedio de edad de los estudiantes.
+// // Calcula el promedio de edad de los estudiantes.
 
-let contador = estudiantes2.length
-console.log(contador);
+// let contador = estudiantes3.length
+// console.log(contador);
 
-let inicio = 0
+// let inicio = 0
 
-estudiantes2.forEach(elemento => {
-    inicio += elemento.edad;
-    return inicio
-})
-console.log(`la suma de las edades de los estudiantes2 es de ${inicio}`);
+// estudiantes3.forEach(elemento => {
+//     inicio += elemento.edad;
+//     return inicio
+// })
+// console.log(`la suma de las edades de los estudiantes3 es de ${inicio}`);
 
-let totalPromedio = inicio / contador;
-console.log(`el promedio de las edades es de ${totalPromedio.toFixed(2)}`);
+// let totalPromedio = inicio / contador;
+// console.log(`el promedio de las edades es de ${totalPromedio.toFixed(2)}`);
 
-// forma alterna en la que pude lograrlo a la primera:
+// // forma alterna en la que pude lograrlo a la primera:
 
-// let prom = estudiantes2.reduce((acc,el) =>
+// let prom = estudiantes3.reduce((acc,el) =>
 //     acc + el.edad,0
 // )
 
-// let total = prom / estudiantes2.length
+// let total = prom / estudiantes3.length
 // console.log(total);
+
+// // task 4
+
+const estudiantes4 = [
+  {
+    nombre: "Ana",
+    edad: 20,
+    promedio: 85,
+    genero: "Femenino",
+    carrera: "Ingeniería Civil",
+    ciudad: "Bogotá",
+  },
+  {
+    nombre: "Juan",
+    edad: 22,
+    promedio: 78,
+    genero: "Masculino",
+    carrera: "Medicina",
+    ciudad: "Medellín",
+  },
+  {
+    nombre: "María",
+    edad: 21,
+    promedio: 90,
+    genero: "Femenino",
+    carrera: "Administración de Empresas",
+    ciudad: "Cali",
+  },
+  {
+    nombre: "Pedro",
+    edad: 23,
+    promedio: 82,
+    genero: "Masculino",
+    carrera: "Derecho",
+    ciudad: "Barranquilla",
+  },
+  {
+    nombre: "Laura",
+    edad: 24,
+    promedio: 88,
+    genero: "Femenino",
+    carrera: "Arquitectura",
+    ciudad: "Cartagena",
+  },
+  {
+    nombre: "Carlos",
+    edad: 20,
+    promedio: 75,
+    genero: "Masculino",
+    carrera: "Ingeniería de Sistemas",
+    ciudad: "Santa Marta",
+  },
+  {
+    nombre: "Sofía",
+    edad: 22,
+    promedio: 95,
+    genero: "Femenino",
+    carrera: "Psicología",
+    ciudad: "Pereira",
+  },
+];
+
+// // Imprime en la consola el nombre y la edad del estudiante con el promedio más alto.
+
+// let studentList = estudiantes4[0];
+
+// let highAverage = estudiantes4.filter((student) => {
+//     student.promedio > studentList;
+//     studentList = student;
+
+// })
+// console.log(studentList);
+
+
+
+
+// // Filtra los estudiantes mayores de 20 años en un nuevo array llamado estudiantesMayores.
+
+let  olderStudents = [];
+
+const studentsTwenty = estudiantes4
+.filter(student => student.edad > 20)
+.map(student => student.nombre)
+
+olderStudents = olderStudents.concat(studentsTwenty) //.concat para concatenar
+
+console.log(olderStudents);
+
+
+
+// // Imprime en la consola la información completa de los estudiantes mayores de 20 años.
+
+
+// let upperToTwenty = estudiantes4.filter((student) => {   // utilizando retun y llaves da lo mismo, que quitar los parentesis, llaves y return
+//     return student.edad > 19
+//     });
+
+// console.log(upperToTwenty);
