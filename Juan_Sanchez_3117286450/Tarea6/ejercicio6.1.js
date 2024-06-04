eedfef02ce6ab9a64fb2c14586bc1e9d2e9c4dcd
Juan Diego Sánchez <Jdsch10.diegosanches11@gmail.com>
2024-06-04 18:57:08 -0500

    Se agrega ejercicio6.1 de la tarea 6

---
+++ Juan_Sanchez_3117286450/Tarea6/ejercicio6.1.js
@@ -0,0 +1,8 @@
+let arrnumeros = [1, 2, 3, 4, 5];
+let arrnumerosMultiplicados = [];
+
+for (let i = 0; i < arrnumeros.length; i++) {
+  arrnumerosMultiplicados.push(arrnumeros[i] * 2);
+}
+
+console.log("El resultado es:", arrnumerosMultiplicados);
