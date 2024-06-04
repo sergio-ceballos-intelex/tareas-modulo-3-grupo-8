e7e77457f8a2b2e938af98137501c1c3ba9529f0
Dmar40 <dumararanguren@gmail.com>
2024-06-04 12:35:02 -0500

    subo tarea 6

---
+++ DUMAR-ARANGURE-3142728582/tarea6/arrays.js
@@ -0,0 +1,87 @@
+// // ## Punto 1
+
+let number = [1, 2, 3, 4, 5]; 
+let total = 2;
+
+for (let number = 1; number <=5; number++) {
+    let total = number * 2;
+    console.log("El resultado de " , number , " * 2 es: " , total);
+}
+
+console.log("--------------------------------------");
+// 
+
+
+
+// // ##Punto 2
+
+let productos = [
+    { nombre: "Camisa", precio: 20 },
+    { nombre: "Pantalón", precio: 30 },
+    { nombre: "Zapatos", precio: 50 },
+  ];
+
+  let precioMasBajo =productos[0];
+  for(let element = 0; element <= productos.length; element++ ){
+    if( precioMasBajo > productos[element]){
+
+      precioMasBajo = productos[element]
+    }
+}
+console.log(precioMasBajo);
+
+console.log("--------------------------------------");
+    
+
+// ##Punto 3.
+
+let estudiantes = [
+  { nombre: "Juan", edad: 20 },
+  { nombre: "María", edad: 22 },
+  { nombre: "Pedro", edad: 19 },
+];
+
+  for(let año = 0; año < estudiantes.length; año++ ){
+    
+    estudiantes[año].edad ++;
+}
+console.log(estudiantes);
+
+console.log("--------------------------------------");
+
+
+// // ##Punto4.
+
+let libros = [
+  { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+  { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+  { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+];
+const letraBuscada = 'E';
+const libroBuscado = [];
+
+for (let index = 0; index < libros.length; index++) {
+  const element = libros[index].titulo.charAt(0).toUpperCase();
+  if (element===letraBuscada) {
+    libroBuscado.push(libros[index]);
+  }
+}
+
+console.log(libroBuscado);
+
+console.log("--------------------------------------");
+
+
+// ##Punto 5.
+
+let frutas1 = ["Manzana", "Plátano", "Naranja"];
+let frutas2 = ["Fresa", "Uva", "Piña"];
+const resultado= frutas1.concat(frutas2);
+console.log(resultado);
+
+
+
+
+
+
+      
---
+++ FELIPE-CANO-1037573104/Tarea 2/README-LAPTOP-M12CRBP3.md
@@ -0,0 +1,2 @@
+Felipe Cano - 1037573104 
+Tarea 2 - Pipecanoc86
