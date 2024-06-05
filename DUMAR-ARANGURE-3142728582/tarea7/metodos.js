5e225900c325a995229ed59ad8d644c8cd94c1b2
Dmar40 <dumararanguren@gmail.com>
2024-06-05 16:43:27 -0500

    subo tarea 7

--- DUMAR-ARANGURE-3142728582/tarea7/metodos.js
+++ DUMAR-ARANGURE-3142728582/tarea7/metodos.js
@@ -1,70 +1,69 @@
 // ##PUNTO 1
 
-// const productos = [
-//     { nombre: "Camiseta", precio: 20, stock: 10 },
-//     { nombre: "Pantalón", precio: 30, stock: 5 },
-//     { nombre: "Zapatos", precio: 50, stock: 0 },
-//     { nombre: "Bufanda", precio: 15, stock: 8 },
-// ];
+const productos = [
+    { nombre: "Camiseta", precio: 20, stock: 10 },
+    { nombre: "Pantalón", precio: 30, stock: 5 },
+    { nombre: "Zapatos", precio: 50, stock: 0 },
+    { nombre: "Bufanda", precio: 15, stock: 8 },
+];
 // - Utilizando filter, genera un nuevo array que contenga los productos en stock (todos los productos cuyo stock es mayor a cero).
 
-// const productoStock = productos.filter(elemento => elemento.stock > 0)
+const productoStock = productos.filter(elemento => elemento.stock > 0);
 
-// console.log(productoStock);
+console.log(productoStock);
+
 //   - Utilizando map, genera un nuevo array con solo los nombres de los productos, ejemplo `["Camiseta", "Pantalón", "Zapatos", "Bufanda" ]`
 
-// const nombreProducto = productos.map((element) => element.nombre);
+const nombreProducto = productos.map((element) => element.nombre);
 
-// console.log(nombreProducto);
+console.log(nombreProducto);
 
 //   - (Opcional) Escribe un programa que calcule el precio total de los productos disponibles, para ello debes recorrer todos los productos y obtener la suma de el precio de todos ellos
+let sumaPrecioTotal = 0;
+productos.forEach((producto) => {
+    sumaPrecioTotal += producto.precio;
+});
 
-
+console.log('la suma del precio total es :',sumaPrecioTotal)
 // ##PUNTO 2
 
 // - Imprime en la consola el nombre del segundo estudiante.
 // - Actualiza la edad del primer estudiante a 25 años.
 //   Imprime en la consola la información completa del primer estudiante después de la actualización.
-// const estudiantes = [
-//     { nombre: "Camilo", edad: 33, promedio: 10 },
-//     { nombre: "Juan", edad: 30, promedio: 6 },
-//     { nombre: "Andres", edad: 29, promedio: 8.7 },
-// ];
+const estudiantes = [
+    { nombre: "Camilo", edad: 33, promedio: 10 },
+    { nombre: "Juan", edad: 30, promedio: 6 },
+    { nombre: "Andres", edad: 29, promedio: 8.7 },
+];
 
-// console.log(estudiantes[1]);
-
-// estudiantes[0].edad=25
-
-// console.log(estudiantes[0]);
+console.log(estudiantes[1]);
+estudiantes[0].edad=25
+console.log(estudiantes[0]);
 
 
 // ##PUNTO 3.
 // Calcula el promedio de edad de los estudiantes.
 // Imprime en la consola tanto la suma como el promedio de edad de los estudiantes.
 
-// let sumaEdades = 0;
-// estudiantes.forEach((estudiante) => {
-//     sumaEdades += estudiante.edad;
-// });
-// console.log(sumaEdades);
+let sumaEdades = 0;
+estudiantes.forEach((estudiante) => {
+    sumaEdades += estudiante.edad;
+});
+console.log(sumaEdades);
 
 
-// let calcularPromedio = 0;
-// estudiantes.forEach((estudiante) => {
-//     calcularPromedio += estudiante.promedio;
-// });
-// console.log(calcularPromedio);
+let calcularPromedio = 0;
+estudiantes.forEach((estudiante) => {
+    calcularPromedio += estudiante.promedio;
+});
+console.log(calcularPromedio);
 
 
 // ##PUNTO 4.
 
 // Utiliza filter para encontrar al estudiante con el promedio más alto en el array.
-// - Imprime en la consola el nombre y la edad del estudiante con el promedio más alto.
-// - Filtra los estudiantes mayores de 20 años en un nuevo array llamado estudiantesMayores.
-// - Imprime en la consola la información completa de los estudiantes mayores de 20 años.
 
-
-const estudiantes = [
+const estudiante2 = [
   {
     nombre: "Ana",
     edad: 20,
@@ -123,11 +122,18 @@ const estudiantes = [
   },
 ];
 
-const promedioMasAlto = estudiantes.filter(elemento => elemento.promedio >= 95);
-
-console.log(promedioMasAlto);
-
 // Utiliza filter para encontrar al estudiante con el promedio más alto en el array.
 // - Imprime en la consola el nombre y la edad del estudiante con el promedio más alto.
+
+const estudiantePromedioAlto = estudiante2.find((max,estudiante) => (estudiante.promedio > max.promedio ? estudiante : max));
+
+console.log(estudiantePromedioAlto);
+console.log(estudiantePromedioAlto.nombre);
+console.log(estudiantePromedioAlto.edad);
+
 // - Filtra los estudiantes mayores de 20 años en un nuevo array llamado estudiantesMayores.
 // - Imprime en la consola la información completa de los estudiantes mayores de 20 años.
+
+const estudiantesMayores = estudiante2.filter(estudiante => estudiante.edad > 20);
+
+console.log(estudiantesMayores);
