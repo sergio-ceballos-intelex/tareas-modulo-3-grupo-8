f0b9854de9f46cbaa82742a6c839725e5f265977
Juan Diego Sánchez <Jdsch10.diegosanches11@gmail.com>
2024-06-04 19:19:14 -0500

    Se agrega ejercicio6.5 de la tarea 6

---
+++ Juan_Sanchez_3117286450/Tarea6/ejercicio6.5.js
@@ -0,0 +1,8 @@
+let frutas1 = ["Manzana", "Plátano", "Naranja"];
+let frutas2 = ["Fresa", "Uva", "Piña"];
+
+for (let i = 0; i < frutas2.length; i++) {
+  frutas1.push(frutas2[i]);
+}
+
+console.log(frutas1);
