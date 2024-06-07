104530f6b38d0dd51f038a36644ee1202d7d4107
Juan_Diego <juan3312orrego@gmail.com>
2024-06-06 23:32:00 -0500

    Punto 2 Callbacks para Filtrar Elementos de un Arreglo. (Y ajustes en el contenido del arreglo numeros).

--- juan-orrego-3172709065/tarea-8/main.js
+++ juan-orrego-3172709065/tarea-8/main.js
@@ -5,9 +5,9 @@ const mensaje = (num) => {
 
 // Punto 1 Callbacks en Manejo de Datos:
 mensaje(1);
-const numeros = [1, 2, 3, 4, 5];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function sumarArreglo(arreglo, callback){
+const sumarArreglo = (arreglo, callback) => {
     // let sumArray = numeros.reduce((total,numeros) => total + numeros, 0);
     let suma = 0;
     arreglo.forEach(element => {
@@ -16,6 +16,19 @@ function sumarArreglo(arreglo, callback){
     callback(suma);
 };
 
-sumarArreglo(numeros, function (resultado) {
-    console.log(resultado); // Debería imprimir 15
-});
+sumarArreglo(numeros, (resultado) => console.log(resultado)); // (Debería imprimir 15, como agregue números hasta el 10 cambia el valor a 55);
+mensaje(2);
+
+// Punto 2 Callbacks para Filtrar Elementos de un Arreglo.
+const arrFiltred = (arreglo, callback) => {
+    let newArray = [];
+    arreglo.forEach(element => {
+        if(callback(element)){
+            newArray.push(element);
+        }
+    })
+    return newArray;
+}
+
+const numPares = arrFiltred(numeros, (par) => par % 2 === 0);
+console.log(numPares);
