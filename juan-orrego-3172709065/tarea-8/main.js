ac57ccf01079f0ec276bb013714fcc2f23d1a055
Juan_Diego <juan3312orrego@gmail.com>
2024-06-07 20:44:51 -0500

    Punto 4 Callback asíncrono: Simulacion vuelo desde Colombia hasta Corea del Sur.

--- juan-orrego-3172709065/tarea-8/main.js
+++ juan-orrego-3172709065/tarea-8/main.js
@@ -5,7 +5,7 @@ const mensaje = (num) => {
 
 // Punto 1 Callbacks en Manejo de Datos:
 mensaje(1);
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numeros = [1, 2, 3, 4, 5];
 
 const sumarArreglo = (arreglo, callback) => {
     // let sumArray = numeros.reduce((total,numeros) => total + numeros, 0);
@@ -23,7 +23,7 @@ mensaje(2);
 const arrFiltred = (arreglo, callback) => {
     let newArray = [];
     arreglo.forEach(element => {
-        if(callback(element)){
+        if (callback(element)) {
             newArray.push(element);
         }
     })
@@ -35,13 +35,46 @@ console.log(numPares);
 mensaje(3);
 
 // Punto 3 Callbacks: Filtrar Números Pares.
+const newNumbers = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 const filtrarPares = (arreglo, callback) => {
-    newArray = arreglo.filter(num => num % 2 === 0);
-    callback(newArray)
+    const newArray = arreglo.filter(num => num % 2 === 0);
+    callback(newArray);
 }
 
-const  mostrarPares = (newArray) => {
-    console.log(newArray);
+const mostrarPares = (newArray) => console.log(newArray);
+
+filtrarPares(newNumbers, mostrarPares);
+mensaje(4);
+
+// Punto 4 Callback asíncrono: Simulacion vuelo desde Colombia hasta Corea del Sur
+console.log("Estoy en Bogota");
+const inBogToMadrid = (callback) => {
+    setTimeout(() => {
+        console.log("Estoy en Bogota - Madrid, me demoré 7 horas");
+        console.log("Llegué a Madrid");
+        callback()
+    }, 7000);
 }
 
-filtrarPares(numeros, mostrarPares)
+const inMadridToFrank = (callback) => {
+    setTimeout(() => {
+        console.log("Vuelo Madrid - Frankfurt, me demoré 2 horas")
+        console.log("Llegué a Frankfurt")
+        callback()
+    }, 2000);
+}
+
+const inFrankToSeul = (callback) => {
+    setTimeout(() => {
+        console.log("Vuelo Frankfurt - Seul, me demoré 10 horas")
+        callback();
+    }, 10000);
+}
+
+inBogToMadrid(()=> {
+    inMadridToFrank(()=> {
+        inFrankToSeul(()=>{
+            console.log("llegue a Seul.");
+        });
+    });
+});
