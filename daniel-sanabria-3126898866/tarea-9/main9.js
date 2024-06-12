8679e1ca95ad0a81ef2792c687f13de9d09800f8
Pininita <pininita92001@gmail.com>
2024-06-12 00:18:01 -0500

    realizando tarea-9, punto uno listo, punto 2 sin terminar

--- daniel-sanabria-3126898866/tarea-9/main9.js
+++ daniel-sanabria-3126898866/tarea-9/main9.js
@@ -1,1 +1,68 @@
-git stat
+const isLogged = true;
+
+const users = [
+  {
+    id: 1,
+    role: 'USER',
+    personId: 10
+  },
+  {
+    id: 2,
+    role: 'USER',
+    personId: 11
+  }
+];
+
+const persons = [
+  {
+    personId: 10,
+    name: 'Rodrigo',
+    lastname: 'Lopez'
+  },
+  {
+    personId: 11,
+    name: 'Jaime',
+    lastname: 'Diaz'
+  },
+  {
+    personId: 15,
+    name: 'Leonardo',
+    lastname: 'Benitez'
+  },
+];
+
+
+function getUsers(usuario) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (usuario) {
+            resolve(users)
+        }else {
+            reject("No estas logueado dentro de la app")
+        }
+    }, 2000);
+  });
+}
+
+getUsers(isLogged)
+.then(res => console.log(res))
+.catch(err => console.log(err))
+
+
+// segundo punto
+
+const anyUser = users[1].personId
+
+
+function getPerson(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const findPerson = persons.find(user => user.personId === anyUser)
+        if (findPerson) {
+            resolve(findPerson)
+        }else{
+            reject("no se encontro el usuario")
+        }
+    }, 5000);
+  });
+}
